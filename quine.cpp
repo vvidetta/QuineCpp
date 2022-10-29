971b2197e725f39dd62863bae9fc3f5251a83f2b
Vito Videtta <vito.videtta@globalgraphics.com>
2022-10-29 13:58:41 +0100

    C++ implementation using printf

--- quine.cpp
+++ quine.cpp
@@ -1,6 +1,20 @@
+
 // Quine
+#include <iostream>
 
 int main()
 {
+  auto program = R"(
+// Quine
+#include <iostream>
+
+int main()
+{
+  auto program = %s%s%c%c;
+  std::printf(program, "R\"(", program, ')', '"');
+  return 0;
+}
+)";
+  std::printf(program, "R\"(", program, ')', '"');
   return 0;
 }
